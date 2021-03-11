78864daec1745e4c5e1d0d5c576a20b93433a1e5
Ikilonchic <43388441+Ikilonchic@users.noreply.github.com>
2021-03-11 23:07:48 +0200

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import {
   BrowserRouter,
   Switch,
   Route,
-  Redirect
+  Redirect,
+  Link,
  } from 'react-router-dom';
-import './App.css';
+import styles from './App.module.scss';
 
+import Header from './components/Header/Header';
 import AlbumPage from './components/AlbumPage/AlbumPage';
 import PhotoPage from './components/PhotoPage/PhotoPage';
 import HomePage from './components/HomePage/HomePage';
@@ -13,20 +15,27 @@ import HomePage from './components/HomePage/HomePage';
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path='/albums/:id' exact>
-          <AlbumPage />
-        </Route>
-        <Route path='/photos/:id' exact>
-          <PhotoPage />
-        </Route>
-        <Route path='/photos' exact>
-          <HomePage />
-        </Route>
-        <Route>
-          <Redirect to='/photos' />
-        </Route>
-      </Switch>
+      <div className={styles['container']}>
+        <Header>
+          <Link to="/photos">Home</Link>
+        </Header>
+        <main className={styles['container__main']}>
+          <Switch>
+            <Route path='/albums/:id' exact>
+              <AlbumPage />
+            </Route>
+            <Route path='/photos/:id' exact>
+              <PhotoPage />
+            </Route>
+            <Route path='/photos' exact>
+              <HomePage />
+            </Route>
+            <Route>
+              <Redirect to='/photos' />
+            </Route>
+          </Switch>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
